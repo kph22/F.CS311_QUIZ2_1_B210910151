f38f39444e9f72ac8d41feef8f6225f1ea71052c
Admin <Admin@DESKTOP-P8RVAUA>
2023-05-03 14:09:02 +0800

    asd

---
+++ src/dsa/Course.java
@@ -0,0 +1,18 @@
+package dsa;
+
+
+public class Course { 
+	public String courseName;
+	
+	Course(String courseName) { 
+		this.courseName = courseName;
+	}
+	public String getCourseName() { 
+		return courseName;
+	}
+	
+	public void setCourseName(String courseName) { 
+		this.courseName = courseName;
+	}
+}
+
